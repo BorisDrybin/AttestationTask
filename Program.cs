4069be87d786fea3ce4680701aa03d32e895747a
Boris Drybin <makoh93@gmail.com>
2023-06-06 15:23:43 +0300

    PrintArray method created

--- Program.cs
+++ Program.cs
@@ -25,3 +25,12 @@ void OverwriteArray(string[] stringArray1, string[] stringArray2)
             }
         }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    Console.WriteLine();
+}
