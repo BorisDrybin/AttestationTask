df5a3f84dba9bfd9a6b6b9817f16713c7a27e2f4
Boris Drybin <makoh93@gmail.com>
2023-06-06 15:29:24 +0300

    small text formattings

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ int m = Convert.ToInt32(Console.ReadLine());
 string[] stringArray1 = new string[m];
 string[] stringArray2 = new string[stringArray1.Length];
 
-void FillArray(string [] stringArray)
+void FillArray(string[] stringArray)
 {
-    for (int i = 0;i<stringArray.Length;i++)
+    for (int i = 0; i < stringArray.Length; i++)
     {
-        Console.Write($"Введите {i+1} элемент массива: ");
+        Console.Write($"Введите {i + 1} элемент массива: ");
         stringArray[i] = Console.ReadLine();
     }
 }
@@ -17,21 +17,21 @@ void OverwriteArray(string[] stringArray1, string[] stringArray2)
 {
     int count = 0;
     for (int i = 0; i < stringArray1.Length; i++)
+    {
+        if (stringArray1[i].Length <= 3)
         {
-            if(stringArray1[i].Length <= 3)
-            {
-                stringArray2[count] = stringArray1[i];
-                count++;
-            }
+            stringArray2[count] = stringArray1[i];
+            count++;
         }
+    }
 }
 
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        {
-            Console.Write($"{array[i]} ");
-        }
+    {
+        Console.Write($"{array[i]} ");
+    }
     Console.WriteLine();
 }
 
