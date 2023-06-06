9555c5d7d0d49ea26e60621d5bbeec162bb8058d
Boris Drybin <makoh93@gmail.com>
2023-06-06 15:21:04 +0300

    Array1 and Array2 created

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+string[] stringArray1 = new string[m];
+string[] stringArray2 = new string[stringArray1.Length];
