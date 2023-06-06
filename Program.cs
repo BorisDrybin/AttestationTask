b12bb82dee7b622c564790a80f6ded4838f40c77
Boris Drybin <makoh93@gmail.com>
2023-06-06 15:21:59 +0300

    FillArray method created

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@ int m = Convert.ToInt32(Console.ReadLine());
 
 string[] stringArray1 = new string[m];
 string[] stringArray2 = new string[stringArray1.Length];
+
+void FillArray(string [] stringArray)
+{
+    for (int i = 0;i<stringArray.Length;i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива: ");
+        stringArray[i] = Console.ReadLine();
+    }
+}
